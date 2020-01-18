dc6753d0aa41fd1fb50f3cae786be3dabe950435
codexvn <24852067+Codexvn@users.noreply.github.com>
2020-01-18 15:57:00 +0800

    2020年 01月 18日 星期六 15:57:00 CST

--- BASIC-13/main.cpp
+++ BASIC-13/main.cpp
@@ -1,21 +1,17 @@
 #include<iostream>
-#include<cstdlib>
+#include<algorithm>
 using namespace std;
-int array[200]={0},N;
-int compare(const void* a, const void* b){
-	 if(*(int*)a>*(int*)b)return 1;
-	 else if(*(int*)a<*(int*)b)return -1;
-	 else return 0;
-}
 int main()
 {
+	int N;
+	int array[200]{0};
 	cin>>N;
 	for(int i=0;i<N;i++)
 		cin>>array[i];
-	qsort(array,N,sizeof(int),compare);
-		for(int i=0;i<N;i++)
-			{
-				cout<<array[i];
-					if(i!=N-1)cout<<' ';
-			}
+		
+	sort(array,array+N);
+	cout<<array[0];
+	for(int i=1;i<N;i++)
+		cout<<' '<<array[i];
+	return 0;
 }
