246ec53dee8299e44f9e8d85acc7261cfb038a5c
codexvn <24852067+Codexvn@users.noreply.github.com>
2020-01-21 15:21:00 +0800

    2020年 01月 21日 星期二 15:21:00 CST

--- BASIC-9/main.cpp
+++ BASIC-9/main.cpp
@@ -1,17 +1,22 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 int main()
 {
 	int n;
 	cin>>n;
+	vector<int>fore,five;
 	for(int i=1;i<=9;i++)
 		for(int j=0;j<=9;j++)
 			for(int k=0;k<=9;k++)
+				{
 				if(2*(i+j)+k==n)
-					cout<<i<<j<<k<<j<<i<<endl;
-		for(int i=1;i<=9;i++)
-		for(int j=0;j<=9;j++)
-			for(int k=0;k<=9;k++)
+					fore.push_back(i*10001+j*1010+k*100);
 				if(2*(i+j+k)==n)
-					cout<<i<<j<<k<<k<<j<<i<<endl;
+					five.push_back(i*100001+j*10010+k*1100);
+					}
+	for(vector<int>::iterator start =fore.begin();start!=fore.end();start++ )
+		cout<<*start<<endl;
+	for(vector<int>::iterator start =five.begin();start!=five.end();start++ )
+		cout<<*start<<endl;
 }
