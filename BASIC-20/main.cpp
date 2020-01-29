30c196b312146ab346ed57f0a2512503d1a23310
codexvn <24852067+Codexvn@users.noreply.github.com>
2020-01-29 18:47:16 +0800

    2020年 01月 29日 星期三 18:47:16 CST

--- BASIC-20/main.cpp
+++ BASIC-20/main.cpp
@@ -3,71 +3,39 @@
 using namespace std;
 string shu_zi[] = { "ling","yi","er","san","si","wu","liu","qi","ba","jiu" }, \
 dan_wei[] = { "","shi","bai","qian" };//"wan","yi"
-void change(int end, int yu) {
+string get;
+vector<string>change;
+inline void Function(int end,string yv) {
+	static bool zeroed =true;
+	bool a=false;
+	for (int count = 0; count <= 3 and end >= 0; end--, count++) {
+		if (get[end] != '0'){
+			zeroed = false;
+			if (a == false and yv!="")change.push_back(" "+yv);
+				a = true;
+		}
 
+		if (zeroed == false)
+			if(get[end] == '0')
+				change.push_back(" ling");
+			else
+				if (count > 0)
+					change.push_back(" " + shu_zi[get[end] - '0'] + " " + dan_wei[count]);
+				else
+					change.push_back(" " + shu_zi[get[end] - '0']);
+
+		if (get[end] == '0')
+			zeroed = true;
+	}  //end:末尾数在get中的下标
+	
 }
 int main() {
-	string get;
-	cin >> get;
-	vector<string>change;
-	bool zeroed =true;//判断是否已经处理过0    
-	for (int end = get.length() - 1, count = 0; count <= 3 and end >= 0; end--, count++) {
-		if (get[end] != '0')
-			zeroed = false;
-		if (zeroed == false)
-			if(get[end] == '0')
-				change.push_back(" ling");
-			else
-				if (count > 0)
-					change.push_back(" " + shu_zi[get[end] - '0'] + " " + dan_wei[count]);
-				else
-					change.push_back(" " + shu_zi[get[end] - '0']);
 
-		if (get[end] == '0')
-			zeroed = true;
-	}  //end:末尾数在get中的下标 
-	bool wan = false;
-	for (int end = get.length() - 5, count = 0; count <= 3 and end >= 0; end--, count++) {
+	cin >> get;   
+	Function(get.length() - 1,"");
+	Function(get.length() - 5,"wan");
+	Function(get.length() - 9,"yi");
 
-		if (get[end] != '0') {
-			zeroed = false;
-			if (wan == false)change.push_back(" wan");
-			wan = true;
-		}
-
-		if (zeroed == false)
-			if(get[end] == '0')
-				change.push_back(" ling");
-			else
-				if (count > 0)
-					change.push_back(" " + shu_zi[get[end] - '0'] + " " + dan_wei[count]);
-				else
-					change.push_back(" " + shu_zi[get[end] - '0']);
-
-		if (get[end] == '0')
-			zeroed = true;
-	}  //end:末尾数在get中的下标
-	bool yi = false;
-	for (int end = get.length() - 9, count = 0; count <= 3 and end >= 0; end--, count++) {
-
-		if (get[end] != '0') {
-			zeroed = false;
-			if (yi == false)change.push_back(" yi");
-			yi = true;
-		}
-
-		if (zeroed == false)
-			if(get[end] == '0')
-				change.push_back(" ling");
-			else
-				if (count > 0)
-					change.push_back(" " + shu_zi[get[end] - '0'] + " " + dan_wei[count]);
-				else
-					change.push_back(" " + shu_zi[get[end] - '0']);
-
-		if (get[end] == '0')
-			zeroed = true;
-	}  //end:末尾数在get中的下标
 	if (change.front() == " ling")
 		change.erase(change.begin());
 	if(change.back()==" yi shi")
