2bef3615854d0006af33e70f7f0d7dc14e46d171
codexvn <24852067+Codexvn@users.noreply.github.com>
2020-02-02 16:25:53 +0800

    2020年 02月 02日 星期日 16:25:53 CST

--- BASIC-27/main.cpp
+++ BASIC-27/main.cpp
@@ -2,8 +2,8 @@
 #include<cstdlib>
 using namespace std;
 int data[9][9]={0},book[9][9]={0},flag[2][9][2],count=0;
-inline void BlackDFS(int step,int n,int i,int j);
-inline void DFS(int step,int n,int i,int j);
+inline void BlackDFS(int step,int n);
+inline void DFS(int step,int n);
 bool FlagCheck(int n,int flag[9][2])
 {
 	for(int i=1;i<=n-1;i++)
@@ -17,44 +17,41 @@ bool FlagCheck(int n,int flag[9][2])
 	return true;
 					 	
 }
-inline void DFS(int step,int n,int i,int j)
+inline void DFS(int step,int n)
 {
 	if(step>n){
-			BlackDFS(1,n,1,1);
+			BlackDFS(1,n);
 			return;
 	}
 	 else 
-		for(;i<=n;i++,j=0)
-			for(;j<=n;j++){
-				if(data[i][j]==1 and book[i][j] != 1){
-					book[i][j]=1;
-					flag[0][step][0]=i; 	
+		for(int j=1;j<=n;j++)
+				if(data[step][j]==1 and book[step][j] != 1){
+					book[step][j]=1;
+					flag[0][step][0]=step; 	
 					flag[0][step][1]=j; 
 					if(FlagCheck(step,flag[0]))	
-					DFS(step+1,n,i,j);
-					book[i][j]=0;
+					DFS(step+1,n);
+					book[step][j]=0;
 				} else
-					continue;
-		}	
+					continue;	
 }
-inline void BlackDFS(int step,int n,int i,int j)
+inline void BlackDFS(int step,int n)
 {
 	if(step>n){
 			count++;
 			return;
 	}
 	 else 
-		for(;i<=n;i++,j=0)
-			for(;j<=n;j++){
-				if(data[i][j]==1 and book[i][j] != 1){
-					book[i][j]=1;
-					flag[1][step][0]=i; 	
-					flag[1][step][1]=j; 
-					if(FlagCheck(step,flag[1]))	
-						BlackDFS(step+1,n,i,j);
-					book[i][j]=0;
-				} else
-					continue;
+		for(int j=0;j<=n;j++){
+			if(data[step][j]==1 and book[step][j] != 1){
+				book[step][j]=1;
+				flag[1][step][0]=step; 	
+				flag[1][step][1]=j; 
+				if(FlagCheck(step,flag[1]))	
+					BlackDFS(step+1,n);
+				book[step][j]=0;
+			} else
+				continue;
 		}	
 }
 int main (){
@@ -66,7 +63,7 @@ int main (){
 			{
 				cin>>data[i][j];
 			}
-	DFS(1,n,1,1);
+	DFS(1,n);
 	//for(int i=n;i>=1;i--)count/=i;
 //	for(int i=n;i>=1;i--)count/=i;
 	cout << count;
