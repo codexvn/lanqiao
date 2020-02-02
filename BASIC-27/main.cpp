5efec718ea90aa819c17c35d93e45b58f093d234
codexvn <24852067+Codexvn@users.noreply.github.com>
2020-02-02 14:38:08 +0800

    2020年 02月 02日 星期日 14:38:08 CST

--- BASIC-27/main.cpp
+++ BASIC-27/main.cpp
@@ -1,7 +1,75 @@
-#include <iostream>
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+int data[9][9]={0},book[9][9]={0},flag[2][9][2],count=0;
+inline void BlackDFS(int step,int n);
+inline void DFS(int step,int n);
+bool FlagCheck(int n,int flag[9][2])
+{
+	for(int i=1;i<=n-1;i++)
+		for(int j=i+1;j<=n;j++)
+			if(flag[i][0] == flag[j][0] \ 
+			 or flag[i][1] == flag[j][1]\
+			 or abs(flag[i][0]-flag[j][0])== abs(flag[i][1]-flag[j][1]) 
+			 ) 
+			 	return false;
+	
+	return true;
+					 	
+}
+inline void DFS(int step,int n)
+{
+	if(step>n){
+		if(FlagCheck(n,flag[0])){
+			BlackDFS(1,n);
+			return;
+		}
+	}
+	 else 
+		for(int i=1;i<=n;i++)
+			for(int j=1;j<=n;j++){
+				if(data[i][j]==1 and book[i][j] != 1){
+					book[i][j]=1;
+					flag[0][step][0]=i; 	
+					flag[0][step][1]=j; 	
+					DFS(step+1,n);
+					book[i][j]=0;
+				} else
+					continue;
+		}	
+}
+inline void BlackDFS(int step,int n)
+{
+	if(step>n){
+		if(FlagCheck(n,flag[1])){
+			count++;
+			return;
+		}
+	}
+	 else 
+		for(int i=1;i<=n;i++)
+			for(int j=1;j<=n;j++){
+				if(data[i][j]==1 and book[i][j] != 1){
+					book[i][j]=1;
+					flag[1][step][0]=i; 	
+					flag[1][step][1]=j; 	
+					BlackDFS(step+1,n);
+					book[i][j]=0;
+				} else
+					continue;
+		}	
+}
+int main (){
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char *argv[]) {
-	return 0;
+	int n;
+	cin>>n;
+	for(int i=1;i<=n;i++)
+		for(int j=1;j<=n;j++)
+			{
+				cin>>data[i][j];
+			}
+	DFS(1,n);
+	for(int i=n;i>=1;i--)count/=i;
+	for(int i=n;i>=1;i--)count/=i;
+	cout << count;
 }
