0f57b53c36427b84ca2f23d6142cbf2bc2c20ff8
codexvn <24852067+Codexvn@users.noreply.github.com>
2020-08-21 16:37:48 +0800

    2020年 08月 21日 星期五 16:37:48 CST

--- ALGO-19/main.cpp
+++ ALGO-19/main.cpp
@@ -6,7 +6,8 @@ using namespace std;
 int main(){
 	std::ios::sync_with_stdio(false);
 	int n,a,b,c;
-	int dp[N+1][N+1][N+1][N+1]={0},data[N+1][N+1]={0};
+	int dp[2*N+1][N+1][N+1]={0},data[N+1][N+1]={0};//dp[这是第几步][第一个人所在的行数][第二个人所在的行数]
+													//因为步数是相同的，所以如果在同一行，则2个人走到了同一个地方 
 	cin>>n;
 	while(true){   //接收数据 
 		cin>>a>>b>>c;
@@ -14,25 +15,23 @@ int main(){
 			break;
 		data[a][b]=c;	
 		}
-	dp[1][1][1][1]=data[1][1];
-	for(int x1 = 1;x1 <=n ;++x1){
-		for (int y1=1;y1<=n;++y1){
-			for(int x2=1;x2<=n;++x2){
-				for(int y2=1;y2<=n;++y2){
-					if(x1+y1!=x2+y2)
-						continue;	//如果不是走了相同多的步数直接进入下一个循环
-					dp[x1][y1][x2][y2]=max(max(dp[x1-1][y1][x2-1][y2],dp[x1-1][y1][x2][y2-1]),max(dp[x1][y1-1][x2-1][y2],dp[x1][y1-1][x2][y2-1])); 
-					//dp[x1][y1][x2][y2]为即将到达[x1][y1]与即将到达[x2][y2]的上一步能取到的最优值之和 
-					//[x1][y1]和[x2][y2]各自有2种状态 
-					if(x1==x2 && y1==y2) //如果走到了一起则只取一次值
-						dp[x1][y1][x2][y2]+=data[x1][y1];
-					else
-					dp[x1][y1][x2][y2]+=data[x1][y1]+data[x2][y2]; //否则加2个点的值 
-				}
-			}
-		}
+	dp[1][1][1]=data[1][1];
+	for(int foot = 2;foot<=2*n-1;++foot){ //右下角，所以要走2*n步
+		 for(int x1=1;x1<=n && x1<=foot;++x1){ 
+		 	for(int x2=1;x2<=n && x2<=foot;++x2){
+		 		int y1=foot-x1+1,y2=foot-x2+1;
+		 		if(x1==x2)//走到了同一个地方则只取一次节点的值 
+					dp[foot][x1][x2]=data[x1][y1];
+				else
+					dp[foot][x1][x2]=data[x1][y1]+data[x2][y2];
+				
+				dp[foot][x1][x2]+=max(max(dp[foot-1][x1-1][x2-1],dp[foot-1][x1-1][x2]),max(dp[foot-1][x1][x2-1],dp[foot-1][x1][x2]));
+				  //dp[foot][x1][x2]表示在第foot步时2个人分别走到[x1][foot-x1]和[x2][foot-x2]的值之和 
+				cout<<foot<<" "<<x1 <<" "<<x2<<" "<<dp[foot][x1][x2]<<endl;
+		 	}
+		 }
 	}
-	cout<<dp[n][n][n][n];
+	cout<<dp[2*n-1][n][n]; 
 	return 0;
 	
 }
