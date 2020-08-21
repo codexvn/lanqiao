4754d567c757d0817e1e095b60291eeea0f72c93
codexvn <24852067+Codexvn@users.noreply.github.com>
2020-08-21 18:00:39 +0800

    2020年 08月 21日 星期五 18:00:39 CST

--- ALGO-20/main.cpp
+++ ALGO-20/main.cpp
@@ -1,15 +1,17 @@
 #include<iostream>
 using namespace std;
 void PrintTree(string mid, string post){
-	string::iterator mid_root=mid.find(*post.rend());
-	string::iterator post_root_left=post.find(*mid_root-1);
-	string::iterator post_root_right=post.find(*mid_root+1);
-	PrintTree(string(mid,begin(),mid_root),string(post,begin(), post_root_left));
-	PrintTree(string(mid_root+1,mid,end()),string(post_root_right,));
+    int mid_root =mid.find(*post.rbegin());
+    if(mid.size()>0){
+        cout<<*post.rbegin();
+        PrintTree(string(mid.begin(),mid.begin()+mid_root),string(post.begin(),post.begin()+mid_root));
+        PrintTree(string(mid.begin()+mid_root+1,mid.end()),string(post.begin()+mid_root,post.end()-1));
+    }
 }
 int main(){
-	std::ios::sync_with_stdio(false);
-	string pre,mid,post;
-	cin>>mid>>post;
-	PrintTree(mid,post);
+    std::ios::sync_with_stdio(false);
+    string pre,mid,post;
+    cin>>mid>>post;
+    PrintTree(mid,post);
 }
+
